fa5b0c508c7c57783d6b5010481ecd12e51f4c21
davisilvas <morg.daviss@gmail.com>
2024-11-25 17:51:57 -0300

    got router working. Starting the about page.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+// pages import
+import HomePage from './pages/HomePage.tsx'
+import About from './pages/About.tsx'
+
 import './index.css'
-import App from './App.tsx'
+//import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+]);
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import HomeBanner from '../components/HomeBanner'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <HomeBanner />
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
